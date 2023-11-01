3a757d6ddb2c71e9be7a34369de2f8089f440ccb
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-11-01 19:49:58 -0300

    Add_class2_part2

--- Leccion01/ecommerce2022/client/js/index.js
+++ Leccion01/ecommerce2022/client/js/index.js
@@ -1,4 +1,5 @@
 const shopContent = document.getElementById("shopContent");
+const cart = []; // Este es nuestro carrito, un array vacio
 
 products.forEach((product) => {
     const content = document.createElement("div");
@@ -7,7 +8,22 @@ products.forEach((product) => {
         <img src="${product.img}">
         <h3>${product.productName}</h3>
         <p>${product.price} $</p>
-    `;
-
+    `
     shopContent.append(content);
+
+    const buybutton = document.createElement("button");
+    buybutton.innerText = "Comprar";
+
+    content.append(buybutton);
+
+    buybutton.addEventListener("click", () => {           
+        cart.push({
+        id: product.id,
+        productName: product.productName,
+        price: product.price,
+        quanty: product.quanty,
+        img: product.img,
+        })
+        console.log(cart)
+    })   
 });
