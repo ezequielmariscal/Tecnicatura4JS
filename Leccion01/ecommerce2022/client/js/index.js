c3c9f97e6553449dc338aaa2298af133af252393
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-11-01 22:01:34 -0300

    Add_class3_part3

--- Leccion01/ecommerce2022/client/js/index.js
+++ Leccion01/ecommerce2022/client/js/index.js
@@ -16,14 +16,23 @@ products.forEach((product) => {
 
     content.append(buybutton);
 
-    buybutton.addEventListener("click", () => {           
-        cart.push({
-        id: product.id,
-        productName: product.productName,
-        price: product.price,
-        quanty: product.quanty,
-        img: product.img,
-        })
-        console.log(cart)
-    })   
+    buybutton.addEventListener("click", () => {
+        const repeat = cart.some((repeatproduct) => repeatproduct.id === product.id);
+        if (repeat) {
+            cart.map((prod) => {
+                if (prod.id === product.id) {
+                    prod.quanty += 1;
+                    displayCartCounter();
+                }
+            });
+        } else {            
+            cart.push({
+            id: product.id,
+            productName: product.productName,
+            price: product.price,
+            quanty: product.quanty,
+            img: product.img,
+            });
+        }
+    });                  
 });
