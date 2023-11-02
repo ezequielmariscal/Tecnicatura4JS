c6a61a365e1de108c2eb00294086dca2fe3525f2
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-11-01 23:25:28 -0300

    Add_class4_part4

---
+++ Leccion01/ecommerce2022/server/server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mercadopago = require("mercadopago");
+
+
+// REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
+mercadopago.configure({
+	access_token: "TOKEN MP",
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());S
+
+app.use(express.static(path.join(__dirname, "../client"))); 
+app.use(cors());S
+
+app.get("/", function () {
+	path.resolve(__dirname, "..","client","index.html");
+}); 
+
+app.post("/create_preference", (req, res) => {
+
+	let preference = {
+		items: [
+			{
+				title: req.body.description,
+				unit_price: Number(req.body.price),
+				quantity: Number(req.body.quantity),
+			}
+		],
+		back_urls: {
+			"success": "http://localhost:8080", // resultado exitoso
+			"failure": "http://localhost:8080", // falla
+			"pending": "http://localhost:8080", // pendiente
+		},
+		auto_return: "approved",
+	};
+
+	mercadopago.preferences
+        .create(preference)
+		.then(function (response) {
+			res.json({
+				id: response.body.id
+			});
+		}).catch(function (error) {
+			console.log(error);
+		});
+});
+
+app.get('/feedback', function (req, res) {
+	res.json({
+		Payment: req.query.payment_id,
+		Status: req.query.status,
+		MerchantOrder: req.query.merchant_order_id
+	});
+});
+
+app.listen(8080, () => {
+	console.log("The server is now running on Port 8080");
+});
