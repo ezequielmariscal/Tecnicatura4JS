974e3947366970b07bddea672ce75a4bea698490
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-11-03 00:46:00 -0300

    Add_class5_part8

--- Leccion02/PERN-stack/src/app.js
+++ Leccion02/PERN-stack/src/app.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(morgan("dev"));
 
-app.get("/", (req,res) => res.json({ message: "Bienvenido a proyecto de zequi"}))
+app.get("/", (req,res) => res.json({ message: "Bienvenido a proyecto de zequi"}));
 
+
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        status: "error",
+        message: err.message
+    });
+});
 export default app;
