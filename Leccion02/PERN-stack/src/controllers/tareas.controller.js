e09019c769612b24469cb30a89e5dbd3adac798f
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-11-08 22:40:11 -0300

    Add_class6_part4

--- Leccion02/PERN-stack/src/controllers/tareas.controller.js
+++ Leccion02/PERN-stack/src/controllers/tareas.controller.js
@@ -6,11 +6,19 @@ export const listarTarea = (req, res) => res.send('obteniendo tarea unica');
 
 export const crearTarea = async(req, res) => {
     const { titulo, descripcion } = req.body;
-    res.send('creando tarea');
+    
 
-    const {rows} = await pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2)', [titulo,descripcion]);
+    try {   const {rows} = await pool.query('INSERT INTO tareas ( titulo, descripcion ) VALUES ($1, $2)', [titulo ,descripcion]);
     console.log(rows);
+    res.send('Creando tarea');
+    } catch (error) {
+        if (error.code === '23505') {
+            return res.send('Ya existe una tarea con ese titulo');
+        }
+        console.log(error);
+    } return res.send("algo salio mal");
+    
 }
 export const actualizarTarea = (req, res) => res.send('actualizando tarea unica');
 
-export const eliminarTarea = (req, res) => res.send('eliminadno tarea unica');
+export const eliminarTarea = (req, res) => res.send('eliminando tarea unica');
